194ab796fba14102c836d2afe0f3dd6f9b6a6f5b
PedroHenriquevr <pedrohvribeiro@gmail.com>
2023-09-06 15:36:08 -0300

    Update index.tsx

--- pages/TopContent/index.tsx
+++ pages/TopContent/index.tsx
@@ -1,17 +1,37 @@
-import { ListDashes } from "phosphor-react-native";
-import { View, Text, StyleSheet } from "react-native";
+import { Faders, MagnifyingGlass } from "phosphor-react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity } from "react-native";
 
 export default function TopContent() {
+  const [text, onChangeText] = useState('');
     return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>Ola, Pedro!</Text>
-        <View>
-          <View style={styles.icons}>
-          <ListDashes size={32} />
-          <Text style={styles.textIcons}>Sua Receita</Text>
+        <Text style={styles.welcome}>OmniPharm</Text>
+        <View style={styles.buscadorContainer}>
+          <View style={styles.buscador}>
+            <MagnifyingGlass size={32} />
+            <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            placeholder="Buscar Produto"
+            />
           </View>
-        </View>
+          <View style={styles.icon}>
+           <Faders size={32} color="#fff" />
+          </View>
       </View>
+      <View style={styles.buscadorContainer}>
+      <TouchableOpacity style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Todos</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Analgesicos</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Vitaminas</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
     )
 } 
 
@@ -24,27 +44,51 @@ const styles = StyleSheet.create({
     gap: 10,
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'left',
+    fontWeight: 'bold'
   },
-  icons: {
+  buscador: {
     display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
     alignItems: 'center',
-    width: 150,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    zIndex: 2,
-    shadowColor: '#171717',
-    shadowOffset: {width: -2, height: 4},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
+    justifyContent: 'space-between',
+    padding: 12,
+    flexDirection: 'row',
+    backgroundColor: '#dedede',
+    gap: 10,
+    borderRadius: 20,
+    width: 'auto'
   },
-  textIcons:{
-    color: '#7b88f7ff',
-    fontSize: 16,
+  input: {
+    width: 220,
     fontWeight: 'bold',
+    fontSize: 16,
+  },
+  icon: {
+    backgroundColor: 'black',
+    borderRadius: 18,
+    padding: 10,
+  },
+  buscadorContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  buttonContainer:{
 
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "black",
+    fontWeight: "bold",
+    alignSelf: "center",
   }
 })
